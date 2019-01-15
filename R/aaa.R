8298985d825673f19fd0a746aa5c9c95afffff3f
Mihai Valcu <valcu.mihai@gmail.com>
2019-01-15 18:54:40 +0100

    updated dataset description.

--- R/aaa.R
+++ R/aaa.R
@@ -5,3 +5,16 @@ utils::globalVariables(c('isCluster', 'datetime', 'tenure'))
 
 NULL
 
+#DATASETS 
+
+
+#' @name pesa56511
+#' @title A dataset containing the track of a pectoral sandpiper male. 
+#' @docType data
+#' @format Track
+#' \describe{
+#'   \item{trajectories::Track}{with "connections","data","sp", "time", "endTime" slots}
+#'   \item{quality}{in data slot, is the Argos location quality}
+#' }
+#' @keywords data
+NULL
