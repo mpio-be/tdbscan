cb472dc5ae289e23ea6f35220c5f32a37cd3ebb7
Mihai Valcu <valcu.mihai@gmail.com>
2019-01-29 12:31:30 +0100

    function dt2Track : data.frame to [trajectories]{Track}

--- R/utils.R
+++ R/utils.R
@@ -15,10 +15,10 @@
 #' @importFrom spacetime      STIDF
 #' @importFrom trajectories   Track
 #' @examples
-#' 
+#' require(data.table)
 #' data(pesa56511)
 #' x = data.frame(pesa56511) %>% data.table
-#' o = dt2Track(x, projection = proj4string(pesa56511) )
+#' o = dt2Track(x, projection = sp::proj4string(pesa56511) )
 #' a = tdbscan(pesa56511, eps =6600, minPts   = 8, maxLag = 6, borderPoints = TRUE )
 #' b = tdbscan(o, eps =6600, minPts   = 8, maxLag = 6, borderPoints = TRUE )
 #' identical(a, b)
