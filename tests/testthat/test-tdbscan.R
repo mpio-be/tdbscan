7798d23bb553625f347545a09ac3e24674b9b9f2
krietsch <jkrietsch@orn.mpg.de>
2019-03-29 19:09:53 +0100

    added testthat for tdbscan

---
+++ tests/testthat/test-tdbscan.R
@@ -0,0 +1,15 @@
+context('tdbscan')
+
+require(tdbscan)
+
+data(pesa56511)
+z = tdbscan(pesa56511, eps = 6600, minPts = 8, maxLag = 6, borderPoints = TRUE )
+
+
+# full function is working
+test_that('tdbscan is list', {
+
+  expect_type( z,  'list' )
+
+})
+
