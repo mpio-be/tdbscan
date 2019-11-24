59843eb1c8dab909745086e5808ac697015e4e4c
krietsch <jkrietsch@orn.mpg.de>
2019-11-24 10:31:50 +0100

    added test for dt2track

--- tests/testthat/test-dt2Track.R
+++ tests/testthat/test-dt2Track.R
@@ -3,15 +3,11 @@ context('dt2Track')
 
 x = data.frame(pesa56511) %>% data.table
 o = dt2Track(x, projection = sp::proj4string(pesa56511) )
-a = tdbscan(pesa56511, eps = 6600, minPts = 8, maxLag = 6, borderPoints = TRUE )
-b = tdbscan(o, eps = 6600, minPts = 8, maxLag = 6, borderPoints = TRUE )
-identical(a, b)
 
 # full function is working
-test_that('dt2Track is data.table', {
+test_that('dt2Track is track', {
 
-  expect_s3_class( a, 'data.table' )
-  expect_s3_class( b, 'data.table' )
+  expect_s4_class( o, 'Track' )
 
 })
 
